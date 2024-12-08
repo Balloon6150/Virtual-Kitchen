5776caa94df0f9adc9c2ec97fcfdc6bf3dc48f16
Balloon6150 <tristan.loland@gmail.com>
2024-12-07 18:19:53 -0800

    Add files via upload

---
+++ #Diffrent Foods!.py
@@ -0,0 +1,75 @@
+#Diffrent Foods!
+Cuisines = ("FrenchCuisine", "ItalianCusiine", "BritishCusine")
+FrenchCuisine = ["Tartiflette", "Chicken Cordon Bleu", "Baguette"]
+ItalianCuisine = ("Caprese Salad", "Eggplant Parmesan", "Fettuccine Afredo")
+BritishCuisine = ("Lancashire Hotpot", "Bubble and Squeak", "Welsh Rarebit")
+
+CusineChoice = 1
+#French foods
+Food1 = (FrenchCuisine[0])
+Food2 = (FrenchCuisine[1])
+Food3 = (FrenchCuisine[2])
+#italians
+Food5 = (ItalianCuisine[0])
+Food6 = (ItalianCuisine[1])
+Food7 = (ItalianCuisine[2])
+#Britans ami right (:})
+Food9 = (BritishCuisine[0])
+Food10 = (BritishCuisine[1])
+Food11 = (BritishCuisine[2])
+
+print("1 : French Cusine")  
+print("2 : Italian Cuisine")
+print("3 : British Cuisine")
+Choice1 = input("Which type of cuisine do you want to eat?")
+if Choice1 == '1':
+    CusineChoice = 1
+    print("1",Food1),
+    print("2",Food2),
+    print("3",Food3),
+    Choice2 = input("Which Food do you want to see the recipe for?")
+    if Choice2 == "1":
+        print("Ingredients : 1 tablespoon butter 3 pounds whole russet potatoes 12 ounces thick cut bacon, cut into 1/2-inch pieces 2 large onions, thinly sliced 1 teaspoon salt, or to taste½ teaspoon freshly ground black pepper, or to taste 1 pinch cayenne pepper, or to taste ½ cup dry white wine ¾ cup creme fraiche 1 (16 ounce) round Reblochon cheese or other ripe, washed rind cheese")
+        print("Directions : Boil unpeeled potatoes with salt in water over high heat; reduce heat and simmer until potatoes are easily pierced with a knife but are not too soft, 15 to 25 minutes depending on size. Remove potatoes from water to cool. Preheat oven to 375 degrees F (190 degrees C).Cook bacon in a skillet over medium to medium-high heat until cooked but not quite crisp and most of the fat is rendered out, 5 to 7 minutes. Push bacon to one side of the pan and blot up some of the fat with a wadded paper towel. Add onion slices. Season with kosher salt and freshly ground black pepper and cayenne. Cook and stir over medium heat until onions are soft, sweet, and golden, about 6 minutes. Add white wine; cook for 2 minutes and deglaze pan. Remove from heat. Cut wheel of cheese into 2 semi-circles. Place each half cut side down and split in half to form 2 more semi-circles, each with a rind side and creamy cheese side. When potatoes are cool enough to handle, peel off the skin. Slice into fairly thick slices, about 1/3 inch thick. Place a little over half the potato slices onto bottom of a 2-quart shallow baking dish. A little overlapping is fine. Sprinkle with a bit of salt. Pour in bacon/onion/wine mixture and spread over potatoes in an even layer. Arrange the remaining potato slices over the top, overlapping as needed. Spread creme fraiche over the potatoes. Place down cheese pieces, rind side up. Transfer pan to a baking sheet. Bake in center rack of preheated oven until browned and potatoes are tender, 45 to 50 minutes.")
+    else:
+        if Choice2 == "2":
+            print("Prep the Chicken; Cover the chicken cutlets with plastic wrap and pound them with a mallet to create thin, even pieces. Thoroughly season the chicken with salt and pepper. Assemble and Roll; Top each piece of chicken with one slice of Swiss cheese and one slice of ham. Roll the chicken vertically so that the ham and cheese are contained. Secure each roll with a toothpick. Bread and Bake; Arrange the chicken rolls in a prepared baking dish. Sprinkle liberally and evenly with breadcrumbs. Bake until the chicken is no longer pink, top each roll with half a slice of Swiss, and continue baking until the cheese is melted")
+        else:
+            if Choice2 == "3":
+                print("Ingredients: 500 g all purpose flour King Arthur brand is recommended about 3 1/2 cups, using 'scoop and swipe' method. 360 g water about 1 1/2 cups + 1 Tbsp. 10 g salt about 2 tsp. 3 g instant yeast about 1 tsp; also known as Quick Rise or Rapid Rise yeast. 25 g honey about 1 Tbsp")
+                print("Instructions: Mix all ingredients in a bowl, cover and let rest for 15 minutes. Over a period of 1 1/2 hours, do 3 sets of stretch and folds, flipping the dough upside down after each set. Cover the bowl and place in refrigerator overnight for about 12-14 hours. Turn the dough on a lightly floured work surface. Divide into 3 equal parts and shape into rectangles. Cover and let rest for 45-60 minutes. Stretch each dough rectangle slightly and fold into a cylinder, sealing the seams. Using your hands, roll the cylinders gently stretching them to desired length, about 14-15 inches. Place on a lightly floured couche, seam side up. Cover and proof at a room temperature for about 30-60 minutes, or until the dough has sufficiently proofed. Starting preheating the oven to 500F, with a baking stone positioned in the upper half the oven and a bread pan filled with hot water on the bottom rack. Transfer the baguettes to a piece of parchment paper, seam side down and dust off excess flour. Using a bread lame, a sharp knife or a razor blade, make 3 scores on each baguette. When scoring, use a swift and firm motion to ensure nice and clean cuts. Open the oven, taking caution not to get burned by steam, and slide the baguettes off onto the baking stone. Close the oven and reduce temperature to 475F. Bake for 15 minutes. Remove the water pan, rotate the baguettes, drop the temperature to 450F and continue baking for another 15 minutes or until deep golden brown.")
+else:
+    if Choice1 == '2':
+        CusineChoice = 2
+        print("1",Food5),
+        print("2",Food6),
+        print("3",Food7),
+        Choice2 = input("Which Food do you want to see the recipe for?")
+        if Choice2 == "1":
+            print("Ingredients: 1 pint mixed cherry tomatoes, preferably heirloom, halved. 7 Tbsp. (or more) extra-virgin olive oil, divided. Flaky sea salt 2 pounds mixed medium and large tomatoes, preferably heirloom, cut into thin slices and/or wedges. 8 oz. buffalo mozzarella or mozzarella, room temperature, torn into pieces. Coarsely ground black pepper. Small basil leaves and toasted country bread (for serving)")
+            print("Preparation: Step 1 Toss 1 pint mixed cherry tomatoes, preferably heirloom, halved with 1 Tbsp. extra-virgin olive oil in a small bowl; season with flaky sea salt. Step 2 Arrange 2 pounds mixed medium and large tomatoes, preferably heirloom, cut into thin slices and/or wedges on a serving platter, slightly overlapping; season generously with flaky sea salt. Arrange 8 oz. buffalo mozzarella or mozzarella, room temperature, torn into pieces over tomatoes; lightly season mozzarella with flaky sea salt. Spoon cherry tomatoes over salad and drizzle with 6 Tbsp. extra-virgin olive oil; season with coarsely ground black pepper. Let stand 30 minutes to let flavors meld and release juices from tomatoes and mozzarella. Step 3 Top salad with small basil leaves and additional salt and oil, if desired. Serve with toasted country bread alongside.")
+        else:
+            if Choice2 == "2":
+                print("Marinara.  cup extra-virgin olive oil. 1 head of garlic, cloves crushed 1 large red onion, chopped 3 oilpacked anchovy fillets (optional. ½ tsp. crushed red pepper flakes 1 Tbsp. tomato paste ¼ cup dry white wine 2 (28oz.) cans whole peeled tomatoes ¼ cup torn basil leaves ½ tsp. dried oregano Kosher salt Eggplant and Assembly 4 lb. Italian eggplants (about 4 medium eggplants), peeled, sliced lengthwise ½ thick Kosher salt 3 cups panko breadcrumbs 1½ tsp. dried oregano 1 tsp. freshly ground black pepper 1½ cups finely grated Parmesan, divided 1½ cups (187.5 g) all-purpose flour 5 large eggs, beaten to blend 1⅓ cups extra-virgin olive oil, divided 1/2 cup finely chopped basil and parsley, plus basil leaves for serving 6 oz. low-moisture mozzarella cheese, grated (about 1⅓ cups) 8 oz. fresh mozzarella cheese, thinly sliceMarinara ¼ cup extra-virgin olive oil1 head of garlic, cloves crushed 1 large red onion, chopped 3 oil-packed anchovy fillets (optional) ½ tsp. crushed red pepper flakes 1 Tbsp. tomato paste ¼ cup dry white wine 2 (28oz.) cans whole peeled tomatoes ¼ cup torn basil leaves ½ tsp. dried oregano Kosher salt Eggplant and Assembly 4 lb. Italian eggplants (about 4 medium eggplants), peeled, sliced lengthwise ½"" thick Kosher salt 3 cups panko breadcrumbs 1½ tsp. dried oregano 1 tsp. freshly ground black pepper 1½ cups finely grated Parmesan, divided 1½ cups (187.5 g) all-purpose flour 5 large eggs, beaten to blend 1⅓ cups extra-virgin olive oil, divided ½ cup finely chopped basil and parsley, plus basil leaves for serving 6 oz. lowmoisture mozzarella cheese, grated (about 1⅓ cups) 8 oz. fresh mozzarella cheese, thinly slice")
+            else:
+                if Choice2 == "3":
+                    print("Bro Im not putting that many things lol!")
+    else:
+        if Choice1 == '3':
+            CusineChoice = 3
+            print("1",Food9),
+            print("2",Food10),
+            print("3",Food11),
+            Choice2 = input("Which Food do you want to see the recipe for?")
+            if Choice2 == "1":
+                print("Bro Im not putting that many things lol!")
+            else:
+                if Choice2 == "2":
+                    print("Bro Im not putting that many things lol!")
+                else:
+                    if Choice2 == "3":
+                        print("Bro Im not putting that many things lol!")
+                    else:
+                        print("Write either 1, 2, or 3")
+
+
